461e4d09edf742a97306b2e17ce6c9757adae245
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2022-05-03 03:32:27 +0900

    SECRET_KEY変更

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(qsq1ne1iscd!jnpd!q$9^8^sw5jfur93os3obr#=s#xlaoyk!'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
